78c9d69de9c22e96dcfaaa019e2bacf7d4511a96
Nor_Book <thanawat.srit@kmutt.ac.th>
2023-03-08 16:33:31 +0700

    Add Project

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -13,18 +13,38 @@ public class Fractal : MonoBehaviour
     [SerializeField]
     private float size;
 
+
     void Start()
     {
-        //  This statement create a game object given a prefab, position and rotation.
-        Instantiate(
-            //  This parameter sets a prefab for an instance to be created.
-            cubePrefab,
+        CreateSponge(iteration, Vector3.zero, size);
+    }
 
-            //  This parameter sets an instance position to (0, 0, 0).
-            Vector3.zero,
-
-            //  This parameter sets an instance rotation to (0, 0, 0).
-            Quaternion.identity
-        );
+    void CreateSponge(int level, Vector3 center, float size)
+    {
+        if (level == 1)
+        {
+            GameObject cube = Instantiate(cubePrefab, center, Quaternion.identity);
+            cube.transform.localScale = Vector3.one * size;
+            cube.transform.parent = transform;
+        }
+        else
+        {
+            float newSize = size/3f;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int z = -1; z <= 1; z++)
+                    {
+                        if (Mathf.Abs(x) + Mathf.Abs(y) + Mathf.Abs(z) > 1)
+                        {
+                            Vector3 offset = new Vector3(x, y, z) * newSize;
+                            Vector3 pos = center + offset;
+                            CreateSponge(level - 1, pos, newSize);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
